8682f6c562c1631df7c728766429e3a6823a62a5
ScorpyRocks <rakesh.scorpion@gmail.com>
2024-03-17 21:30:27 +0100

    searchComponent added

--- searchCapital/src/app/app.module.ts
+++ searchCapital/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchComponentComponent } from './search-component/search-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponentComponent
   ],
   imports: [
     BrowserModule
